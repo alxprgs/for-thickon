fb2c1fb359f88ff63f6219f4ef4a78b80c18277c
Proshka228328 <proshka20081010@gmail.com>
2025-01-09 17:23:06 +0300

    upd 09.01.2025

--- web/__init__.py
+++ web/__init__.py
@@ -1,10 +1,12 @@
 from motor.motor_asyncio import AsyncIOMotorClient
+from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="web/front"), name="static")
 
 try:
     client = AsyncIOMotorClient(os.getenv("MONGO_URL"))
@@ -14,4 +16,4 @@ except Exception as e:
     print(f"Ошибка подключения к базе данных: {str(e)}")
     raise e
 
-from web.routes import reg, login, check_auth, logout
+from web.routes import reg, login, check_auth, logout, root, onas, contact
