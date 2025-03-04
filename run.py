8050d8acb12e75137f9e0dbd70d4f34c429854eb
Ашот Куликян <133698340+AshKulikyan@users.noreply.github.com>
2025-03-04 23:35:27 +0300

    update

--- run.py
+++ run.py
@@ -4,6 +4,6 @@ import os
 
 if __name__ == "__main__":
     try:
-        uvicorn.run("web:app", port=int(os.getenv("PORT")), host="0.0.0.0")
+        uvicorn.run("web:app", port=int(5003), host="0.0.0.0")
     except Exception as e:
         print(f"error: {e}")
